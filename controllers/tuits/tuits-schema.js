bc71e02651daa22b61478d3083c793801eaee96e
bharath sanjay <bharathsanjay4321@gmail.com>
2023-06-20 20:56:03 -0400

    create tuits

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,6 +3,13 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  dislikes : Number,
+  disliked : Boolean,
+  comments : Number,
+  retweets : Number,
+  image : String,
+  handle : String
+
 }, {collection: 'tuits'});
 export default schema;
 
